fceaf606199096fbc9ea53280de7179f8c7d50fc
Вадим <guts.vadim@gmail.com>
2017-05-03 16:59:19 +0300

    tinyMCE image and source plugin activated

--- backend/views/default/create.php
+++ backend/views/default/create.php
@@ -53,10 +53,12 @@ $this->params['breadcrumbs'][] = $this->title;
                         'clientOptions' => [
                             'plugins' => [
                                 "advlist autolink lists link charmap print preview anchor",
-                                "searchreplace visualblocks fullscreen",
-                                "insertdatetime media table contextmenu paste"
+                                "searchreplace visualblocks code fullscreen",
+                                "insertdatetime media table contextmenu paste",
+                                "image"
                             ],
-                            'toolbar' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+                            'toolbar' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+                            'image_advtab' => true
                         ]
                     ]) ?>
             <?= Html::submitButton(
